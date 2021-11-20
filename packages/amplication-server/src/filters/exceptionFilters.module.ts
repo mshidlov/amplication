4f12673861c0e7c2c36f9da1a4f6a542d89126bc
Matan Shidlov <91742238+mshidlov@users.noreply.github.com>
2021-11-20 19:00:43 +0200

    Update exceptionFilters.module.ts

--- packages/amplication-server/src/filters/exceptionFilters.module.ts
+++ packages/amplication-server/src/filters/exceptionFilters.module.ts
@@ -11,6 +11,7 @@ import { GithubAuthExceptionFilter } from './github-auth-exception.filter';
 
 @Module(
 {
+  kmlkmm
                imports: [ConfigModule, RootWinstonModule],
   providers: [GqlResolverExceptionsFilter, GithubAuthExceptionFilter],
   exports: [GqlResolverExceptionsFilter, GithubAuthExceptionFilter]
