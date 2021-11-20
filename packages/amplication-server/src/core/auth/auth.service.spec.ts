6da136f7f1df6d1f1953462214686119fca9e408
Matan Shidlov <91742238+mshidlov@users.noreply.github.com>
2021-11-20 18:57:57 +0200

    Update auth.service.spec.ts

--- packages/amplication-server/src/core/auth/auth.service.spec.ts
+++ packages/amplication-server/src/core/auth/auth.service.spec.ts
@@ -11,6 +11,8 @@ import { WorkspaceService } from '../workspace/workspace.service';
 import { EnumTokenType } from './dto';
 const EXAMPLE_TOKEN = 'EXAMPLE TOKEN';
 
+
+
 const EXAMPLE_ACCOUNT: Account = {
   id: 'alice',
   email: 'alice@example.com',
